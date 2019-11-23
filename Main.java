3287b1cf635014a732c10fb525331bdf6918ab83
Marcus Twyford <twyfordm@countryday.net>
2019-11-23 20:27:03 +0000

    main method - added comments, to test repl sync

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
   public static void main(String[] args) {
    Register register= new Register();
+   //testing Repl.it integration back and forth in github
+   //at the same time, grading Pelle's work
+   System.out.println("Welcome");
    register.Register();
   }
 }
